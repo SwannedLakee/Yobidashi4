6811603ccb3e5b8e452947b48908fe2f0e9bb562
toastkidjp <10fmi13@gmail.com>
2026-02-16 09:11:18 +0900

    refactor Divide to other class file.

---
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/component/SingleLineTransformation.kt
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2026 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+package jp.toastkid.yobidashi4.presentation.component
+
+import androidx.compose.foundation.text.input.InputTransformation
+import androidx.compose.foundation.text.input.TextFieldBuffer
+
+class SingleLineTransformation : InputTransformation {
+
+    override fun TextFieldBuffer.transformInput() {
+        var indexOf = asCharSequence().indexOf("\n")
+        while (indexOf != -1) {
+            replace(indexOf, indexOf + 1, "")
+            indexOf = asCharSequence().indexOf("\n")
+        }
+    }
+
+}
