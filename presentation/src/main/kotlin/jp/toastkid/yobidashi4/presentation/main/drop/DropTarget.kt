3d53e0c5045d94c4c9d88355dbd8cf38369de50e
toastkidjp <10fmi13@gmail.com>
2026-02-17 09:25:30 +0900

    refactor Add license.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/main/drop/DropTarget.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/main/drop/DropTarget.kt
@@ -1,8 +1,19 @@
+/*
+ * Copyright (c) 2025 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
 package jp.toastkid.yobidashi4.presentation.main.drop
 
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.draganddrop.DragAndDropEvent
 import androidx.compose.ui.draganddrop.DragAndDropTarget
+import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+import org.slf4j.LoggerFactory
 import java.awt.datatransfer.DataFlavor
 import java.awt.datatransfer.UnsupportedFlavorException
 import java.awt.dnd.DnDConstants
@@ -10,11 +21,7 @@ import java.awt.dnd.DropTargetDropEvent
 import java.io.File
 import java.io.IOException
 import java.nio.file.Path
-import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
 import kotlin.io.path.name
-import org.koin.core.component.KoinComponent
-import org.koin.core.component.inject
-import org.slf4j.LoggerFactory
 
 class DropTarget : DragAndDropTarget, KoinComponent {
 
