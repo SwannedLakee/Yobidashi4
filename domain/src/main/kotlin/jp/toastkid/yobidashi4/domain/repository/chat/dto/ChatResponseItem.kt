944375db6cc3b691d2c396fa4a6a3228fc4dfe9a
toastkidjp <10fmi13@gmail.com>
2026-02-19 09:08:38 +0900

    feature Add new field.

--- domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/repository/chat/dto/ChatResponseItem.kt
+++ domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/repository/chat/dto/ChatResponseItem.kt
@@ -1,9 +1,12 @@
 package jp.toastkid.yobidashi4.domain.repository.chat.dto
 
+import jp.toastkid.yobidashi4.domain.model.chat.Source
+
 data class ChatResponseItem(
     private val message: String,
     private val error: Boolean = false,
-    private val image: Boolean = false
+    private val image: Boolean = false,
+    private val sources: List<Source> = emptyList<Source>(),
 ) {
 
     fun message() = message
