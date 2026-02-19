fabb55e8a94eee77d27430ac3737b9c487d33bc6
toastkidjp <10fmi13@gmail.com>
2026-02-19 09:09:19 +0900

    feature Add parse grounding source.

--- infrastructure/src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/service/chat/ChatStreamParser.kt
+++ infrastructure/src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/service/chat/ChatStreamParser.kt
@@ -1,5 +1,6 @@
 package jp.toastkid.yobidashi4.infrastructure.service.chat
 
+import jp.toastkid.yobidashi4.domain.model.chat.Source
 import jp.toastkid.yobidashi4.domain.repository.chat.dto.ChatResponseItem
 import java.util.regex.Pattern
 
@@ -23,6 +24,19 @@ class ChatStreamParser {
             else
                 null
 
+        val sources = mutableListOf<Source>()
+        if (line.contains("groundingChunks")) {
+            val sourceMatcher = sourcePattern.matcher(line)
+            while (sourceMatcher.find()) {
+                sources.add(
+                    Source(
+                        sourceMatcher.group(2),
+                        sourceMatcher.group(1)
+                    )
+                )
+            }
+        }
+
         val imageMatcher = imagePattern.matcher(line)
         val base64 = if (imageMatcher.find()) imageMatcher.group(2) else null
 
@@ -33,7 +47,8 @@ class ChatStreamParser {
 
         return ChatResponseItem(
             message = messageText,
-            image = base64 != null
+            image = base64 != null,
+            sources = sources
         )
     }
 
@@ -42,3 +57,5 @@ class ChatStreamParser {
 private val pattern = Pattern.compile("\\{\"parts\": \\[\\{\"text\": \"(.*?)\"}]", Pattern.DOTALL)
 
 private val imagePattern = Pattern.compile("\"inlineData\":(.+?)\"data\": \"(.+?)\"", Pattern.DOTALL)
+
+private val sourcePattern = Pattern.compile("\\{\"web\": \\{\"uri\": \"(.+?)\",\"title\": \"(.+?)\"}}", Pattern.DOTALL)
